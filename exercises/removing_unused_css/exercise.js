f092e172559737c27d6dadae30da71fb1544e92d
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2015-05-08 23:48:50 -0300

    removing unused css kit in progress..

---
+++ exercises/removing_unused_css/exercise.js
@@ -0,0 +1,106 @@
+'use strict';
+
+var chalk = require('chalk');
+var jsdom = require('jsdom');
+var cheerio = require('cheerio');
+var wat = require('workshopper-wat');
+var jquery = require('../../lib/jquery');
+var exercise = wat({ verify: verify });
+
+module.exports = exercise;
+
+function verify (t, req, res) {
+  var body = res.body;
+  var $ = cheerioOrBail();
+  if ($ === void 0) {
+    return;
+  }
+
+  t.fgroup('sanity');
+
+  var scripts = $('script');
+  if (scripts.length === 0) {
+    t.ffail('empty', { tag: 'script' });
+  }
+  var styles = $('link[rel="stylesheet"]');
+  if (styles.length === 0) {
+    t.ffail('empty', { tag: 'style' });
+  }
+
+  var scriptSources = [];
+  var linkSources = [];
+
+  scripts.each(validateScript);
+  styles.each(validateStyle);
+
+  console.log(exercise.__('boot_jsdom', { process: chalk.yellow('jsdom') }));
+  jsdom.env({
+    html: body,
+    src: [jquery],
+    features: {
+      FetchExternalResources: ['script'],
+      ProcessExternalResources: ['script']
+    },
+    done: domloaded
+  });
+
+  function validateScript () {
+    var el = $(this);
+    var nonblocking = !!el.attr('async');
+    var src = el.attr('src');
+    if (src) {
+      t.fgroup('script', { src: src });
+      if (scriptSources.indexOf(src) !== -1) {
+        t.ffail('dupe', { tag: 'script' }); return;
+      }
+      scriptSources.push(src);
+      t.fpass('body', el.parent().is('body'));
+      t.fpass('bottom', el.nextAll().length === el.nextAll('script').length);
+      t.fpass('async', nonblocking);
+    }
+  }
+
+  function validateStyle () {
+    var el = $(this);
+    var href = el.attr('href');
+    t.fgroup('link', { href: href });
+    t.fpass('noscript', el.parent().is('noscript'));
+    linkSources.push(href);
+  }
+
+  function cheerioOrBail () {
+    try {
+      return cheerio.load(body);
+    } catch (err) {
+      t.end(err);
+    }
+  }
+
+  function domloaded (err, window) {
+    if (err) {
+      t.error(err); return;
+    }
+    var $ = window.$;
+    linkSources.forEach(verifyLink);
+    t.end();
+
+    function verifyLink (href) {
+      t.fgroup('link', { href: href });
+
+      var links = $('link').filter(byLinkAndMedia);
+      if (links.length === 0) {
+        t.ffail('noloadcss');
+      } else if (links.length > 1) {
+        t.ffail('dupe', { tag: 'link' });
+      } else {
+        t.fpass('deferred', links.attr('media') === 'all');
+      }
+
+      function byLinkAndMedia () {
+        var el = $(this);
+        var matches = el.attr('href') === href;
+        return matches;
+      }
+    }
+  }
+}
---
+++ exercises/removing_unused_css/problem.md
@@ -0,0 +1,20 @@
+Hey there! If you haven't yet, make sure to run `perfschool init` _before proceeding_.
+It will create a `perfschool-playground` directory with everything you need to get started.
+
+From time to time, you'll work on a project where you use a CSS framework such as `Bootstrap`.
+Or, you may be working in a legacy project with tons of CSS that you just _know_ it's not being
+used anymore. In these cases there's a nifty little tool you can use to get rid of all those CSS
+rules that are not being put to good use.
+
+This saves your users considerable bytes, especially when it comes to including a framework only
+to get access to a couple of the styles they've defined.
+
+The `uncss` package allows you to get rid of all of those useless bytes by loading a file or URL
+using PhantomJS, a headless browser that's able to interpret resources such as CSS and JavaScript.
+
+# PROBLEM
+
+You are given an application with a bunch of CSS that's used, and a bunch of CSS that's unused.
+Leverage `uncss` to filter out the unused CSS and only serve what's needed.
+
+Don't inline the styles. Instead, have an endpoint return them as a CSS stylesheet.
